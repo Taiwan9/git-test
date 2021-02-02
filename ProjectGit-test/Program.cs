dbe0f3b0687ab601a507aaf41a2eed17f96c3223
Taiwan9 <clownser1994@gmail.com>
2021-02-02 09:56:53 -0300

    projeto iniciado, mostrando ola mundo

---
+++ ProjectGit-test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectGit_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
